32e985a2a352679c595da3c4b8a6aea4e3e01a2a
mohamed zakarya <MOHAMED ZAKARYA>
2024-05-26 18:18:17 +0300

    interface for bookService & userService

--- src/main/java/com/app/service/BookService.java
+++ src/main/java/com/app/service/BookService.java
@@ -1,5 +1,24 @@
 package com.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.app.dto.BookDto;
+
 public interface BookService {
+
+	public List<BookDto> getAllBooks();
+
+	public Optional<BookDto> getBookById(Long id);
+
+	public BookDto CreateBook(BookDto bookDto);
+
+	public BookDto updateBook(BookDto bookDto);
+
+	public String deleteBookById(Long id);
+	
+	public BookDto borrowBook (Long id , BookDto bookDto);
+
+	public BookDto returnBook (Long id , BookDto bookDto);
 
 }
--- src/main/java/com/app/service/UserService.java
+++ src/main/java/com/app/service/UserService.java
@@ -1,5 +1,22 @@
 package com.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.app.dto.UserDto;
+
 public interface UserService {
 
+	
+	public List<UserDto> getAllUsers();
+	
+	public Optional<UserDto> getUserById (Long id);
+	
+	public UserDto CreateUser(UserDto userDto);
+	
+	public UserDto updateUser(UserDto userDto);
+	
+	public String deleteUserById(Long id); 
+	
+	
 }
